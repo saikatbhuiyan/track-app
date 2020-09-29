669ca41d0c495a7b792a294f05b176f3b9836e49
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-09-30 01:45:55 +0600

    Create Like model and mutation for create Likes

--- tracks/schema.py
+++ tracks/schema.py
@@ -1,6 +1,8 @@
 import graphene
 from graphene_django import DjangoObjectType
-from .models import Track
+
+from .models import Track, Like
+from users.schema import UserType
 
 
 class TrackType(DjangoObjectType):
@@ -96,7 +98,31 @@ class DeleteTrack(graphene.Mutation):
         return DeleteTrack(track_id=track_id)
 
 
+class CreateLike(graphene.Mutation):
+    user = graphene.Field(UserType)
+    track = graphene.Field(TrackType)
+
+    class Arguments:
+        track_id = graphene.Int(required=True)
+
+    def mutate(self, info, track_id):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception("Logged in to like.")
+
+        track = Track.objects.get(id=track_id)
+        if not track:
+            raise Exception('Cannot find the track.')
+
+        Like.objects.create(
+            user=user,
+            track=track
+        )
+
+        return CreateLike(user=user, track=track)
+
 class Mutation(graphene.ObjectType):
     create_track = CreateTrack.Field()
     update_track = UpdateTrack.Field()
     delete_track = DeleteTrack.Field()
+    create_like = CreateLike.Field()
