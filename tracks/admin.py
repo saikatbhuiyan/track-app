d3607065ffd4b88504daa5285909227167b52e3f
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-09-29 06:37:15 +0600

    create track project and tracks app Track model , get track Query and exicution

---
+++ tracks/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Track
+
+@admin.register(Track)
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'created_at', ]
---
+++ tracks/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Track(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
