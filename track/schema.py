04b30aa95cd60c9306f2be38d02cec4aa90dad32
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-09-29 07:22:16 +0600

    Create CreateTrack Mutation for track

--- track/schema.py
+++ track/schema.py
@@ -5,5 +5,8 @@ import tracks.schema
 class Query(tracks.schema.Query, graphene.ObjectType):
     pass
 
+class Mutation(tracks.schema.Mutation, graphene.ObjectType):
+    pass
 
-schema = graphene.Schema(query=Query)
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- tracks/schema.py
+++ tracks/schema.py
@@ -10,6 +10,32 @@ class TrackType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     tracks = graphene.List(TrackType)
-    
+
     def resolve_tracks(self, info):
         return Track.objects.all()
+
+
+class CreateTrack(graphene.Mutation):
+    track = graphene.Field(TrackType)
+
+    class Arguments:
+        title = graphene.String()
+        description = graphene.String()
+        url = graphene.String()
+
+    # def mutate(self, info, **kwargs):
+    #     description = kwargs.get('description')
+    #     title = kwargs.get('title')
+    #     url = kwargs.get('url')
+    #     track = Track(title=title, description=description, url=url)
+    #     track.save()
+    #     return CreateTrack(track=track)
+
+    def mutate(self, info, title, description, url):
+        track = Track(title=title, description=description, url=url)
+        track.save()
+        return CreateTrack(track=track)
+
+
+class Mutation(graphene.ObjectType):
+    create_track = CreateTrack.Field()
